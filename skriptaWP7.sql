2fdbdef17eb3379faf7490618a9ebcb71a63fd99
nadklara <klara.nad@gmail.com>
2024-11-24 21:02:41 +0100

    Update skriptaWP7.sql

--- skriptaWP7.sql
+++ skriptaWP7.sql
@@ -1,4 +1,10 @@
 ﻿--ovako se pisu komantari
 
 --ova naredba kreira bazu
-create database edunovawp7;
+--create database edunovawp7;
+
+--use edunova wp7
+
+create table smjer (
+naziv varchar(50)
+);
