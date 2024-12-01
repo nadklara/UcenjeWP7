3c3d5d65374d480e75ba8f7366879e0dd27e0bd4
nadklara <klara.nad@gmail.com>
2024-12-01 23:33:40 +0100

    Kreiranje tablice smjerovi

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -8,6 +8,11 @@ create database edunovawp7;
 go
 use edunovawp7;
 go
-create table smjer(
-naziv varchar(50)
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+trajanje int,
+cijena decimal(18,2),
+vaucer bit,
+izvodiseod datetime
 );
