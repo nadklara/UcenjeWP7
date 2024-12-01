de391fe95a45f2e99386925e077d46473bb112f5
nadklara <klara.nad@gmail.com>
2024-12-01 23:22:15 +0100

    Osnovne naredbe za drop ako postoji i kreiranje baze

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -1,7 +1,13 @@
 ﻿--create database edunovawp7;
 
---use edunovawp7;
-
+use master;
+go
+drop database if exists edunovawp7;
+go
+create database edunovawp7;
+go
+use edunovawp7;
+go
 create table smjer(
 naziv varchar(50)
 );
