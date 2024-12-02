a1e363c0118a54d571afdc93292111f9fe354cfd
nadklara <klara.nad@gmail.com>
2024-12-02 19:32:12 +0100

    Primarni kljucevi

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -8,31 +8,33 @@ create database edunovawp7;
 go
 use edunovawp7;
 go
+
 create table smjerovi(
-sifra int,
-naziv varchar(50),
-trajanje int,
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+trajanje int null, -- null se ne mora pisati
 cijena decimal(18,2),
 vaucer bit,
 izvodiseod datetime
 );
 
 create table polaznici(
-sifra int,
-ime varchar(50),
-prezime varchar(50),
+sifra int not null primary key identity (1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
 oib char(11),
-email varchar(100)
+email varchar(100) not null
 );
 
 create table grupe(
-sifra int,
-naziv varchar(20),
-smjer int,
-predavac varchar(50)
+sifra int not null primary key identity (1,1),
+naziv varchar(20) not null,
+smjer int not null references smjerovi (sifra),
+predavac varchar(50) 
 );
 
 create table clanovi(
-grupa int,
-polaznik int
+grupa int not null references grupe (sifra),
+polaznik int not null references polaznici(sifra)
 );
+
