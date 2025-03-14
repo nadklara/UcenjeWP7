3e2c0bc2203586666e2f85c1b20112d49e9e52cd
nadklara <klara.nad@gmail.com>
2025-03-14 15:26:32 +0100

    Insert naredba

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -37,3 +37,53 @@ create table clanovi(
 grupa int not null references grupe (sifra),
 polaznik int not null references polaznici(sifra)
 );
+
+
+--1
+insert into smjerovi 
+(naziv, trajanje, cijena, vaucer, izvodiseod) values
+('Web programiranje',225,1245.99,1,'2024-09-07 17:00:00');
+
+insert into smjerovi(naziv) values
+--2
+('Java programer'),
+--3
+('Serviser'),
+--4
+('Knjigovodstvo');
+
+insert into grupe (naziv, smjer) values
+('WP6',1),
+('WP7',1),
+('JP27',2),
+('K12',4);
+
+
+INSERT INTO polaznici (ime, prezime, email) VALUES 
+('Ante', 'Janković', 'antejankovic86@gmail.com'),
+('Stojan', 'Carić', 'stojancaric8@gmail.com'),
+('Željko', 'Lučan', 'lucko1987vk@gmail.com'),
+('Petar', 'Gudelj', 'gudelj.petar2005@gmail.com'),
+('Krunoslav', 'Popić', 'kpopic@gmail.com'),
+('Jurica', 'Ognjenović', 'ognjenovicjurica0610@gmail.com'),
+('Lea', 'Bartoš', 'talulea@gmail.com'),
+('Tomislav', 'Nađ', 'tomislav.nadj@gmail.com'),
+('Martin', 'Galik', 'gale1508@gmail.com'),
+('Ivan', 'Mišić', 'ivanmisic983@gmail.com'),
+('Mirjam', 'Koški', 'mir.jam975@gmail.com'),
+('Željko', 'Koški', 'zeljko.koski@gmail.com'),
+('Mirza', 'Delagić', 'mirzadelagic@gmail.com'),
+('Bruno', 'Čačić', 'bruno.cacic@gmail.com'),
+('David', 'Nađ', 'david08.nadj@gmail.com'),
+('Antonio', 'Macanga', 'macanga.antonio@gmail.com'),
+('Nina', 'Zrno', 'ninaradakovic1234@icloud.com'),
+('Marko', 'Berberović', 'marko.berberovic@skole.hr'),
+('Tomislav', 'Nebes', 'tomislav.nebes@gmail.com'),
+('Klara', 'Nađ', 'klara.nad@gmail.com'),
+('Maja', 'Šteler', 'maja5steler@gmail.com'),
+('Milan', 'Drača', 'milan.draca@gmail.com'),
+('Marin', 'Vranješ', 'marinvranjes123@gmail.com'),
+('Boris', 'Bukovec', 'botaosijek@gmail.com'),
+('Luka', 'Jurak', 'jurakluka18@gmail.com'),
+('Ivan', 'Strmečki', 'ivan.strmecki8@gmail.com'),
+('Bruno', 'Bašić', 'brunobasic031@gmail.com');
